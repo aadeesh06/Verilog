348a9709f244af1ff9cfcc2ca660bce3671915cb
Aadeesh <aadeesh.2002@gmail.com>
2023-08-08 22:51:20 +0530

    initial commit

---
+++ serial_parity_detector.v
@@ -0,0 +1,21 @@
+module parity_gen(x,clk,z);
+    //a moore machine
+    input x, clk;
+    output reg z;
+    reg even_odd; // machine state
+    parameter even = 1, odd = 0;
+    always @(posedge clk) begin
+        case(even_odd)
+            even : even_odd <= x ? odd : even;
+            odd : even_odd <= x ? even : odd;
+            default : even_odd <= even;
+        endcase
+    end
+    always @(even_odd)
+    begin
+        case (even_odd)
+            even : z = 0;
+            odd : z = 1;
+        endcase
+    end
+endmodule
---
+++ serial_parity_detector_tb.v
@@ -0,0 +1,20 @@
+`include "serial_parity_detector.v"
+module parity_test;
+    reg clk, x;
+    wire z;
+    parity_gen PAR(x,clk,z);
+    initial begin
+        $dumpfile ("parity.vcd");
+        $dumpvars(0,parity_test);
+        clk = 1'b0;
+    end
+    always
+    #5 clk = ~clk;
+    initial 
+    begin
+        #2 x = 0; #10 x = 1; #10 x = 1; #10 x = 1;
+        #10 x = 0; #10 x = 1; #10 x = 1; #10 x = 0;
+        #10 x = 0; #10 x = 1; #10 x = 1; #10 x = 0;
+        #10 $finish;
+    end
+endmodule
