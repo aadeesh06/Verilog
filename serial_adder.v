d49a31a9e64f849832bec87fbbb39aecb25e5a98
Aadeesh <aadeesh.2002@gmail.com>
2023-08-11 08:26:07 +0530

    Fixed the bug in the code(Changed the expression of sum and PS).

--- serial_adder.v
+++ serial_adder.v
@@ -5,25 +5,30 @@ module serial_adder(a , b, reset,  sum);
     output reg sum;
     reg state; // the only state is previous carry
     parameter s0 = 0, s1 = 1;
-    reg PS, NS;
-    always @( posedge reset)
+    reg PS;
+    // always @(posedge reset)
+    // begin
+    //     if(reset)
+    //         PS <= 0;    
+    //     else 
+    //         PS <= NS;
+    // end
+    always @(*)
     begin
         if(reset)
-            PS <= 0;    
-        else 
-            PS <= NS;
-    end
-    always @(PS, a, b)
-    begin
-        case(PS)
-            s0 : begin
-                    sum  = (a | b) ? 1 : 0;
-                    NS = (a & b) ? s1 : s0;
-                 end
-            s1 : begin
-                    sum = (a ^ b) ? 0 : 1;
-                    NS = (a | b) ? s1 : s0;
-                 end
-        endcase
+            PS <= s0;
+        else
+        begin
+            case(PS)
+                s0 : begin
+                        sum  = (a ^ b);
+                        PS = (a & b) ? s1 : s0;
+                    end
+                s1 : begin
+                        sum = ~(a ^ b);
+                        PS = (a | b) ? s1 : s0;
+                    end
+            endcase
+        end
     end
 endmodule
--- serial_adder_tb.v
+++ serial_adder_tb.v
@@ -10,11 +10,11 @@ module serial_adder_tb;
         $dumpvars(0, serial_adder_tb);
 
         reset = 1'b1;
-        #15 reset = 1'b0;
+        #10 reset = 1'b0;
     end
     initial
     begin
-        #15 a = 0; b = 0;
+        #10 a = 0; b = 0;
         #10 a = 1; b = 1;
         #10 a = 1; b = 0;
         #10 a = 0; b = 0;
