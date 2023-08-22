990f9a7bd211c8f45861d767f6e55e4fc038b1ab
Aadeesh <aadeesh.2002@gmail.com>
2023-08-22 13:38:16 +0530

    Fixed the logical error in code

--- switch_level_modeling/cmos_nand.v
+++ switch_level_modeling/cmos_nand.v
@@ -6,7 +6,7 @@ module cmos_nand(x , y ,f);
     supply0 gnd;  // ground = 0
     wire a;
     pmos p1(f, vdd, x);
-    nmos n1(f, vdd, y);
-    pmos p2(f, a, x);
+    pmos p2(f, vdd, y);
+    nmos n1(f, a, x);
     nmos n2(a, gnd, y);
 endmodule
