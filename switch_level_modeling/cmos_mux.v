b64a4d164daf5e8df9ff43580cee490889eb45bd
Aadeesh <aadeesh.2002@gmail.com>
2023-08-22 13:54:16 +0530

    Comment update

--- switch_level_modeling/cmos_mux.v
+++ switch_level_modeling/cmos_mux.v
@@ -1,4 +1,6 @@
 // This code is of a 2 x 1 multiplexer using cmos switch.
+// For understanding of the code, it is better to see the 2 x 1 multiplexer implementation using transmission gate.
+
 module mux_2to1(out,s, i0, i1);
     input s, i0, i1;
     output out;
