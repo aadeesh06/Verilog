e36a1a921cb8cd0e3afcb3f5d8709e772e51a7e9
Aadeesh <aadeesh.2002@gmail.com>
2023-08-22 13:21:09 +0530

    updated comments

--- switch_level_modeling/cmos_inverter.v
+++ switch_level_modeling/cmos_inverter.v
@@ -1,9 +1,9 @@
-//code for cmos inverter
+//code for cmos inverter using switch level modeling
 module cmos_not(x,f);
-    input x;
-    output f;
-    supply1 vdd;
-    supply0 gnd;
-    pmos p1(f, vdd,x);
-    nmos n1(f,gnd,x);
+    input x; // input variable
+    output f; // output variable
+    supply1 vdd;  // logic high 
+    supply0 gnd; // logic zero
+    pmos p1(f, vdd,x);  // pmos transistor
+    nmos n1(f,gnd,x); // nmos transistor
 endmodule
